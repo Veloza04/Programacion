35a8794c7302ac98bc3d786b838450b1eceee636
Veloza04 <joseduard95@gmail.com>
2023-03-15 21:17:17 -0500

    ejercicio suma

---
+++ Suma/Suma/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Vamos a sumar 2 numeros enteros");
+
+int num1, num2;
+bool numeroValido1, numeroValido2;
+do
+{
+    Console.Write("Ingrese el primer número: ");
+    numeroValido1 = int.TryParse(Console.ReadLine(), out num1);
+    if (!numeroValido1)
+    {
+        Console.WriteLine("Entrada no válida, por favor inténtelo de nuevo.");
+    }
+} while (!numeroValido1);
+do
+{
+    Console.Write("Ingrese el segundo número: ");
+    numeroValido2 = int.TryParse(Console.ReadLine(), out num2);
+    if (!numeroValido2)
+    {
+        Console.WriteLine("Entrada no válida, por favor inténtelo de nuevo.");
+    }
+} while (!numeroValido2);
+int resultado = num1 + num2;
+Console.WriteLine($"La suma de {num1} y {num2} es: " + resultado);
+Console.ReadKey();
