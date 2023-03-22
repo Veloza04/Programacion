e0ad56ac409d66769c75278205585f4c877c1d01
Veloza04 <joseduard95@gmail.com>
2023-03-21 20:39:25 -0500

    Cambios suma

--- Suma/Suma/Program.cs
+++ Suma/Suma/Program.cs
@@ -1,4 +1,4 @@
-﻿Console.WriteLine("Vamos a sumar 2 numeros enteros");
+﻿Console.WriteLine("Vamos a hacer operaciones entre 2 numeros enteros");
 
 int num1, num2;
 bool numeroValido1, numeroValido2;
@@ -20,6 +20,54 @@ do
         Console.WriteLine("Entrada no válida, por favor inténtelo de nuevo.");
     }
 } while (!numeroValido2);
-int resultado = num1 + num2;
-Console.WriteLine($"La suma de {num1} y {num2} es: " + resultado);
+
+bool opcionValida;
+int eleccion;
+double resultado;
+Console.WriteLine("A continuacion elija la operacion que desea realizar:");
+
+do
+{
+    Console.WriteLine("Si deseas hacer suma, oprime 1");
+    Console.WriteLine("Si deseas hacer resta, oprime 2");
+    Console.WriteLine("Si deseas hacer multiplicacion, oprime 3");
+    Console.WriteLine("Si deseas hacer division, oprime 4");
+
+    opcionValida = int.TryParse(Console.ReadLine(), out eleccion);
+    if (eleccion >= 1 && eleccion <= 4)
+    {
+        opcionValida = false;
+    }
+    else
+    {
+        Console.WriteLine("Opcion no valida, intenta de nuevo.");
+    }
+} while (opcionValida);
+switch (eleccion)
+{
+    case 1:
+        resultado = num1 + num2;
+        break;
+    case 2:
+        resultado = num1 - num2;
+        break;
+    case 3:
+        resultado = num1 * num2;
+        break;
+    case 4:
+        if (num2 != 0)
+        {
+            resultado = num1 / num2;
+        }
+        else
+        {
+            Console.WriteLine("No se puede dividir entre 0");
+            return;
+        }
+        break;
+    default:
+        Console.WriteLine("Entrada no válida");
+        return;
+}
+Console.WriteLine($"El resultado de la operacion que elegiste entre {num1} y {num2} es: " + resultado);
 Console.ReadKey();
